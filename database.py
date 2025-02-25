964d9410c8a57c352b685a3a96fffcda9509e2f9
mhkhan <mhkhancou@gmail.com>
2025-02-25 18:24:05 +0600

    added v2

--- database.py
+++ database.py
@@ -1,10 +1,15 @@
 # algoverse/database.py
+from os import getenv
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-Database_Url = "sqlite:///./algoverse.db" 
+load_dotenv()
 
-engine = create_engine(Database_Url, connect_args={"check_same_thread": False})
+SQLALCHEMY_DATABASE_URL = getenv("DATABASE_URL")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
--- routes/JWTtoken.py
+++ routes/JWTtoken.py
@@ -2,10 +2,12 @@ from datetime import datetime, timedelta
 from typing import Optional
 from jose import JWTError, jwt
 from fastapi import HTTPException, status
+from os import getenv
+from dotenv import load_dotenv
 
-import schemas
+load_dotenv()
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+SECRET_KEY = getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
